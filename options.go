9fb1affbfd02818236400157ad82f4a486897eab
yuliang.li <yuliang.li@q2bi.com>
2025-01-14 18:36:31 +0800

    fix: run func interface add data param

--- options.go
+++ options.go
@@ -1,5 +1,7 @@
 package workflowQ
 
+import "time"
+
 type QueueOption func(*Queue)
 
 func (q *Queue) SetOptions(opts ...QueueOption) {
@@ -15,3 +17,4 @@ func (q *Queue) runEmptyQueueFn() {
 	q.emptyQueueFn()
 }
 
+type TimeOptions func() (timeoutSec time.Duration, frequencySec time.Duration)
